648d393b8ade871fab7cd81849c14d1de1ebe4ba
miles <mjm382@bu.edu>
2021-03-11 22:00:37 -0800

    fix site link; new favicon

--- src/components/NavigationMenu.js
+++ src/components/NavigationMenu.js
@@ -3,7 +3,7 @@ import React from 'react'
 function NavigationMenu() {
     return (
         <div className="navigationMenu">
-            toki pona is a philosophical artistic constructed language created by Sonja Lang. site by <a className="siteLink" href="milesmahon.com">miles</a>.
+            toki pona is a philosophical artistic constructed language created by Sonja Lang. site by <a className="siteLink" href="https://milesmahon.com">miles</a>.
         </div>
     )
 }
